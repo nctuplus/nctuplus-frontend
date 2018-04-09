470b75409220cc04e23b04499edfbabbecc58da9
yanglin <yanglin5689446@gmail.com>
2018-04-09 17:11:16 +0800

    fix little indent problem

--- src/js/Components/Pagination/index.js
+++ src/js/Components/Pagination/index.js
@@ -12,33 +12,22 @@ const PageItemLink = ({ onClick, children, active, disabled }) => (
 )
 
 const Pagination = ({ page, maxPage, to }) => (
-
   <ul className='pagination justify-content-center'>
     { /* if page > 1: display 'First' and 'Prev' button */}
-    {
-      page > 1 && <PageItemLink onClick={() => to(1)} >First</PageItemLink> }
+    { page > 1 && <PageItemLink onClick={() => to(1)} >First</PageItemLink> }
     { page > 1 && <PageItemLink onClick={() => to(page - 1)}>Prev</PageItemLink> }
 
     { /* if page > 1: display omit button ('...' button) */}
     { page > 3 && <PageItemLink disabled>...</PageItemLink> }
     { /* display [page -2, page + 2] buttons */}
-    {
-      page > 2 &&
-      <PageItemLink onClick={() => to(page - 2)}>{ page - 2 }</PageItemLink>
-    }
-    {
-      page > 1 &&
-      <PageItemLink onClick={() => to(page - 1)}>{ page - 1 }</PageItemLink>
-    }
+    
+    { page > 2 && <PageItemLink onClick={() => to(page - 2)}>{ page - 2 }</PageItemLink>}
+    { page > 1 && <PageItemLink onClick={() => to(page - 1)}>{ page - 1 }</PageItemLink> }
+
     <PageItemLink active>{ page }</PageItemLink>
-    {
-      page < maxPage &&
-      <PageItemLink onClick={() => to(page + 1)}>{ page + 1 }</PageItemLink>
-    }
-    {
-      page < maxPage - 1 &&
-      <PageItemLink onClick={() => to(page + 2)}>{ page + 2 }</PageItemLink>
-    }
+    
+    { page < maxPage && <PageItemLink onClick={() => to(page + 1)}>{ page + 1 }</PageItemLink> }
+    { page < maxPage - 1 && <PageItemLink onClick={() => to(page + 2)}>{ page + 2 }</PageItemLink> }
 
     { /* if page < maxPage - 2: display omit button ('...' button) */ }
     { page < maxPage - 2 && <PageItemLink disabled>...</PageItemLink> }
