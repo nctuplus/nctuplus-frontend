2e5db4a0a45c1265ad51118687a7f207a06485a1
yanglin <yanglin5689446@gmail.com>
2018-04-14 12:25:51 +0800

    fix footer

--- src/js/Components/Cover/index.js
+++ src/js/Components/Cover/index.js
@@ -1,48 +1,20 @@
 
 import React from 'react'
-import { Transition } from 'react-transition-group'
 import './style.scss'
 
-const duration = 1000
-
 class Cover extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = { in: true, index: 0 }
-    this.changeBackground = this.changeBackground.bind(this)
-  }
-  changeBackground () {
-    // repeatedly change index
-    const state = this.state
-    const props = this.props
-    console.log(state)
-    this.setState({ index: (state.index + 1) % props.images.length, in: false })
-  }
   render () {
-    let url = this.props.images[this.state.index]
     return (
-      <Transition
-        in={this.state.in}
-        timeout={duration}
-        onEntered={() => setTimeout(this.changeBackground, 3000)}
-        onExited={() => this.setState({ in: true })}
-        appear
+      <div
+        className='cover'
+        ref={cover => (this.cover = cover)}
+        style={{
+          background: `url('${this.props.images[0]}') no-repeat`,
+          backgroundSize: 'cover'
+        }}
       >
-        {
-          (status) => (
-            <div
-              className='cover'
-              ref={cover => (this.cover = cover)}
-              style={{
-                transition: `all ${duration}ms ease`,
-                background: (status === 'entered') ? `url("${url}")` : 'black'
-              }}
-            >
-              { this.props.children }
-            </div>
-          )
-        }
-      </Transition>
+        { this.props.children }
+      </div>
     )
   }
 }
--- src/js/Components/Footer/index.js
+++ src/js/Components/Footer/index.js
@@ -1,23 +1,22 @@
 
 import React from 'react'
-import classNames from 'classnames'
 import './style.scss'
 
 const Footer = (props) => (
   /* if current page is login page, the footer should be bottom-fixed. */
-  <footer className={classNames(props.match.params.url === 'login' && 'fixed')} >
-    <p>
-      <span>© Copyright NCTU+ 2016</span>
+  <footer>
+    <div className='text-center'>
+      <span className='mx-2'>© Copyright NCTU+ 2016</span>
       <a href='https://www.facebook.com/nctuplus' target='_blank'>
-        <i className='fa fa-2x fa-facebook-square' />
+        <i className='fa fa-2x fa-facebook-square mx-1' />
       </a>
       <a href='https://github.com/nctuplus/nctuplus/' target='_blank'>
-        <i className='fa fa-2x fa-github-square' />
+        <i className='fa fa-2x fa-github-square mx-1' />
       </a>
       <a href='mailto:nctuplus@gmail.com' target='_blank'>
-        <i className='fa fa-envelope' style={{ fontSize: 28 }} />
+        <i className='fa fa-envelope fa-2x mx-1' />
       </a>
-    </p>
+    </div>
   </footer>
 )
 
