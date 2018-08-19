c3f2016c7fbd830d8d502743325985a3194175a8
Yang Lin <yanglin5689446@gmail.com>
2018-08-19 21:25:08 +0800

    Refactor event#new, implement file upload

--- src/js/Pages/Events/New/index.js
+++ src/js/Pages/Events/New/index.js
@@ -2,12 +2,11 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
-import { Redirect } from 'react-router-dom'
 import CKEditor from 'react-ckeditor-component'
+import { base64encode } from '../../../Utilities'
 import PageWrapper from '../../../Components/PageWrapper'
 import { LabeledInput } from '../../../Components/FormUtils'
 import { postEvent, postEventReset } from '../../../Redux/Actions/Events'
-import { FETCHING_STATUS } from '../../../constants'
 
 class New extends React.Component {
   constructor (props) {
@@ -25,7 +24,10 @@ class New extends React.Component {
     })
   }
 
-  handleSubmit (event) {
+  async handleSubmit (event) {
+    const coverImageUrl = this.refs.cover_image.files[0]
+      ? await base64encode(this.refs.cover_image.files[0])
+      : null
     let payload = {
       event_type: this.refs.event_type.value,
       title: this.refs.title.value,
@@ -34,22 +36,18 @@ class New extends React.Component {
       location: this.refs.location.value,
       begin_time: this.refs.begin_time.value,
       end_time: this.refs.end_time.value,
+      cover_image: coverImageUrl,
       content: this.state.ckeditorContent
     }
 
     if ((payload.title !== '') && (payload.event_type !== '')) {
       // 讓表單不要照預設方法送出
       event.preventDefault()
-      this.props.post_event(payload)
+      this.props.postEvent(payload)
     }
   }
 
   render () {
-    if (this.props.status === FETCHING_STATUS.DONE) {
-      this.props.post_event_reset()
-      return (<Redirect to={`/events/${this.props.event.id}`} />)
-    }
-
     return (
       <PageWrapper>
         <div className='container bg-white'>
@@ -60,22 +58,25 @@ class New extends React.Component {
               </div>
               <form>
                 <LabeledInput label='圖片'>
-                  <input accept='image/*' type='file' hidden />
-                  <div className='text-center clickable upload-picture' >
-                      點選以上傳,建議800x400以達最佳效果(需小於2MB)
+                  <input ref='cover_image' accept='image/*' type='file' hidden />
+                  <div
+                    className='text-center clickable upload-picture'
+                    onClick={() => this.refs.cover_image.click()}
+                  >
+                    點選以上傳,建議800x400以達最佳效果(需小於2MB)
                   </div>
                 </LabeledInput>
                 <LabeledInput label='類型/名稱'>
                   <div className='input-group'>
                     <div className='input-group-prepend'>
                       <select ref='event_type' required>
-                        <option value=''>類型</option>
-                        <option value='校友週'>校友週</option>
-                        <option value='講座'>講座</option>
-                        <option value='表演'>表演</option>
-                        <option value='擺攤'>擺攤</option>
-                        <option value='比賽'>比賽</option>
-                        <option value='其他'>其他</option>
+                        <option value='0'>類型</option>
+                        <option value='1'>校友週</option>
+                        <option value='2'>講座</option>
+                        <option value='3'>表演</option>
+                        <option value='4'>擺攤</option>
+                        <option value='5'>比賽</option>
+                        <option value='6'>其他</option>
                       </select>
                     </div>
                     <input ref='title' className='form-control' placeholder='必填' type='text' required />
@@ -112,7 +113,7 @@ class New extends React.Component {
                   <CKEditor activeClass='p10' content={this.state.ckeditorContent} events={{'change': this.onChange}} />
                 </LabeledInput>
                 <div className='col-12 text-right'>
-                  <button type='submit' className='btn btn-success btn-large' onClick={this.handleSubmit}>送出</button>
+                  <button type='submit' className='btn btn-success btn-large' onClick={(e) => this.handleSubmit(e)}>送出</button>
                 </div>
               </form>
             </div>
@@ -129,8 +130,8 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  post_event: (payload) => dispatch(postEvent(payload)),
-  post_event_reset: () => dispatch(postEventReset())
+  postEvent: (payload) => dispatch(postEvent(payload)),
+  postEventReset: () => dispatch(postEventReset())
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(New))
---
+++ src/js/Utilities/index.js
@@ -0,0 +1,11 @@
+
+function base64encode (file) {
+  return new Promise((resolve, reject) => {
+    const reader = new window.FileReader()
+    reader.readAsDataURL(file)
+    reader.onload = () => resolve(reader.result)
+    reader.onerror = error => reject(error)
+  })
+}
+
+export { base64encode }
