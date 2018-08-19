8206786ae4973d62664b81a3e90ce4f549c3e886
Yang Lin <yanglin5689446@gmail.com>
2018-08-19 20:37:35 +0800

    Refactor EventPreview component

--- src/js/Components/EventPreview/index.js
+++ src/js/Components/EventPreview/index.js
@@ -2,43 +2,40 @@
 import React from 'react'
 import classNames from 'classnames'
 import { Link } from 'react-router-dom'
+import moment from 'moment'
 
-const EventPreview = (props) => {
-  let now = Date.now()
-  let inProgress = Date.parse(props.begin_time) <= now && now <= Date.parse(props.end_time)
-  return (
-    <div className='col-12 col-sm-6 col-md-3 mb-3'>
-      <div className='card bg-white'>
+const EventPreview = (props) => (
+  <div className='col-12 col-sm-6 col-md-3 mb-3'>
+    <div className='card bg-white'>
 
-        <Link to={`/events/${props.id}`}>
-          <img
-            alt='poster'
-            className='card-img-top clickable'
-            src={`${SERVER_URL}${props.cover_image.url}`}
-          />
-        </Link>
+      <Link to={`/events/${props.id}`}>
+        <img
+          alt='poster'
+          className='card-img-top clickable'
+          src={`${SERVER_URL}${props.cover_image.url}`}
+        />
+      </Link>
 
-        <div className='card-text p-2'>
-          <div className='pull-right'>
-            <span className={classNames('bold', 'date', inProgress && 'text-red')}>
-              {
-                inProgress
-                  ? '進行中'
-                  : props.begin_time.slice(0, 10)
-              }
-            </span>
-          </div>
-          <br />
-          <div className='card-title bold'>{ props.title }</div>
+      <div className='card-text p-2'>
+        <div className='pull-right'>
+          <span className={classNames('bold', 'date', moment().isBetween(props.begin_time, props.end_time) && 'text-red')}>
+            {
+              moment().isBetween(props.begin_time, props.end_time)
+                ? '進行中'
+                : props.begin_time.slice(0, 10)
+            }
+          </span>
+        </div>
+        <br />
+        <div className='card-title bold'>{ props.title }</div>
 
-          <div className='info'>
-            <div className='info-item'>單位: { props.organization }</div>
-            <div className='info-item'>地點: { props.location }</div>
-          </div>
+        <div className='info'>
+          <div className='info-item'>單位: { props.organization }</div>
+          <div className='info-item'>地點: { props.location }</div>
         </div>
       </div>
     </div>
-  )
-}
+  </div>
+)
 
 export default EventPreview
