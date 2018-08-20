f05fed976f200e23366f01da58f592400b50cd36
Yang Lin <yanglin5689446@gmail.com>
2018-08-20 16:44:42 +0800

    Add form validation (only works in chrome)

--- src/js/Pages/Events/New/index.js
+++ src/js/Pages/Events/New/index.js
@@ -3,126 +3,12 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import CKEditor from 'react-ckeditor-component'
+import { compose, withState, lifecycle, withHandlers, withProps } from 'recompose'
 import { base64encode } from '../../../Utilities'
 import PageWrapper from '../../../Components/PageWrapper'
 import { LabeledInput } from '../../../Components/FormUtils'
 import { postEvent, postEventReset } from '../../../Redux/Actions/Events'
-
-class New extends React.Component {
-  constructor (props) {
-    super(props)
-    this.onChange = this.onChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.state = {
-      ckeditorContent: ''
-    }
-  }
-
-  onChange (event) {
-    this.setState({
-      ckeditorContent: event.editor.getData()
-    })
-  }
-
-  async handleSubmit (event) {
-    const coverImageUrl = this.refs.cover_image.files[0]
-      ? await base64encode(this.refs.cover_image.files[0])
-      : null
-    let payload = {
-      event_type: this.refs.event_type.value,
-      title: this.refs.title.value,
-      url: this.refs.url.value,
-      organization: this.refs.organization.value,
-      location: this.refs.location.value,
-      begin_time: this.refs.begin_time.value,
-      end_time: this.refs.end_time.value,
-      cover_image: coverImageUrl,
-      content: this.state.ckeditorContent
-    }
-
-    if ((payload.title !== '') && (payload.event_type !== '')) {
-      // 讓表單不要照預設方法送出
-      event.preventDefault()
-      this.props.postEvent(payload)
-    }
-  }
-
-  render () {
-    return (
-      <PageWrapper>
-        <div className='container bg-white'>
-          <div className='row'>
-            <div className='col-md-9 offset-1'>
-              <div className='title offset-1'>
-                新增活動
-              </div>
-              <form>
-                <LabeledInput label='圖片'>
-                  <input ref='cover_image' accept='image/*' type='file' hidden />
-                  <div
-                    className='text-center clickable upload-picture'
-                    onClick={() => this.refs.cover_image.click()}
-                  >
-                    點選以上傳,建議800x400以達最佳效果(需小於2MB)
-                  </div>
-                </LabeledInput>
-                <LabeledInput label='類型/名稱'>
-                  <div className='input-group'>
-                    <div className='input-group-prepend'>
-                      <select ref='event_type' required>
-                        <option value='0'>類型</option>
-                        <option value='1'>校友週</option>
-                        <option value='2'>講座</option>
-                        <option value='3'>表演</option>
-                        <option value='4'>擺攤</option>
-                        <option value='5'>比賽</option>
-                        <option value='6'>其他</option>
-                      </select>
-                    </div>
-                    <input ref='title' className='form-control' placeholder='必填' type='text' required />
-                  </div>
-                </LabeledInput>
-                <LabeledInput label='活動網址'>
-                  <input ref='url' className='form-control' placeholder='選填' type='text' />
-                </LabeledInput>
-                <div className='form-group'>
-                  <div className='row'>
-                    <label className='col-sm-2 control-label text-right' >主辦單位</label>
-                    <div className='col-4'>
-                      <input ref='organization' className='form-control' placeholder='必填' type='text' />
-                    </div>
-                    <label className='col-sm-2 control-label text-right'>地點</label>
-                    <div className='col-4'>
-                      <input ref='location' className='form-control' placeholder='必填' type='text' />
-                    </div>
-                  </div>
-                </div>
-                <div className='form-group'>
-                  <div className='row'>
-                    <label className='col-sm-2 control-label text-right'>開始時間</label>
-                    <div className='col-4'>
-                      <input ref='begin_time' className='form-control' placeholder='必填' type='date' />
-                    </div>
-                    <label className='col-sm-2 control-label text-right'>結束時間</label>
-                    <div className='col-4'>
-                      <input ref='end_time' className='form-control' placeholder='必填' type='date' />
-                    </div>
-                  </div>
-                </div>
-                <LabeledInput label='內容'>
-                  <CKEditor activeClass='p10' content={this.state.ckeditorContent} events={{'change': this.onChange}} />
-                </LabeledInput>
-                <div className='col-12 text-right'>
-                  <button type='submit' className='btn btn-success btn-large' onClick={(e) => this.handleSubmit(e)}>送出</button>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </PageWrapper>
-    )
-  }
-}
+import { FETCHING_STATUS } from '../../../constants'
 
 const mapStateToProps = (state) => ({
   event: state.events.post.data,
@@ -134,4 +20,184 @@ const mapDispatchToProps = (dispatch) => ({
   postEventReset: () => dispatch(postEventReset())
 })
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(New))
+const enhance = compose(
+  withRouter,
+  connect(mapStateToProps, mapDispatchToProps),
+  withState('payload', 'setPayload', {
+    event_type: 0,
+    title: '',
+    url: '',
+    organization: '',
+    location: '',
+    begin_time: '',
+    end_time: '',
+    cover_image: '',
+    content: ''
+  }),
+  withState('fileUploadStatus', 'setFileUploadStatus', 'none'),
+  withState('uploadedImageUrl', 'setUploadedImageUrl', 'none'),
+  withProps(({ formRef: React.createRef(), imageUploadRef: React.createRef() })),
+  withHandlers({
+    updatePayload: ({ setPayload }) => payload => setPayload(previous => ({ ...previous, ...payload })),
+    onFileUpload: ({ setFileUploadStatus, setPayload, imageUploadRef, setUploadedImageUrl }) => () => {
+      setFileUploadStatus('uploading')
+      const file = imageUploadRef.current.files[0]
+      base64encode(file)
+        .then(encoded => {
+          setPayload(previous => ({ ...previous, cover_image: encoded }))
+          setFileUploadStatus('done')
+          setUploadedImageUrl(URL.createObjectURL(file))
+        })
+    },
+    onSubmit: ({ formRef, fileUploadStatus, payload, postEvent }) => event => {
+      // only works on chrome, but who care others? ;)
+      formRef.current.reportValidity()
+      // 讓表單不要照預設方法送出
+      event.preventDefault()
+      // 檔案還沒上傳並編碼完前不送出
+      if (fileUploadStatus === 'uploading') return
+      console.log(payload)
+
+      if (payload.title && payload.event_type && payload.organization && payload.location) {
+        postEvent(payload)
+      }
+    }
+  }),
+  lifecycle({
+    componentDidUpdate: function () {
+      if (this.props.status === FETCHING_STATUS.DONE) {
+        this.props.postEventReset()
+        this.props.history.push(`/events/${this.props.event.id}`)
+      }
+    }
+  })
+)
+
+const New = props => (
+  <PageWrapper>
+    <div className='container bg-white pt-5'>
+      <div className='row'>
+        <div className='col-md-9 offset-1'>
+          <div className='h3 offset-1'>
+            新增活動
+          </div>
+          <form ref={props.formRef}>
+            <LabeledInput label='圖片'>
+              <input
+                ref={props.imageUploadRef}
+                accept='image/*' type='file' hidden
+                onChange={props.onFileUpload}
+              />
+              <div
+                className='text-center clickable upload-picture p-5'
+                onClick={() => props.imageUploadRef.current.click()}
+              >
+                {
+                  props.fileUploadStatus === 'done'
+                    ? <img src={props.uploadedImageUrl} className='img-fluid' />
+                    : '點選以上傳,建議800x400以達最佳效果(需小於2MB)'
+                }
+              </div>
+            </LabeledInput>
+            <LabeledInput label='類型/名稱'>
+              <div className='input-group'>
+                <div className='input-group-prepend'>
+                  <select
+                    value={props.payload.event_type}
+                    onChange={e => props.updatePayload({ event_type: e.target.value })}
+                    required
+                  >
+                    <option value='0'>類型</option>
+                    <option value='1'>校友週</option>
+                    <option value='2'>講座</option>
+                    <option value='3'>表演</option>
+                    <option value='4'>擺攤</option>
+                    <option value='5'>比賽</option>
+                    <option value='6'>其他</option>
+                  </select>
+                </div>
+                <input
+                  value={props.payload.title}
+                  onChange={e => props.updatePayload({ title: e.target.value })}
+                  className='form-control'
+                  placeholder='必填'
+                  type='text'
+                  required
+                />
+              </div>
+            </LabeledInput>
+            <LabeledInput label='活動網址'>
+              <input
+                value={props.payload.url}
+                onChange={e => props.updatePayload({ url: e.target.value })}
+                className='form-control'
+                placeholder='選填'
+                type='text'
+              />
+            </LabeledInput>
+            <div className='form-group'>
+              <div className='row'>
+                <label className='col-sm-2 control-label text-right' >主辦單位</label>
+                <div className='col-4'>
+                  <input
+                    value={props.payload.organization || ''}
+                    onChange={e => props.updatePayload({ organization: e.target.value })}
+                    className='form-control'
+                    placeholder='必填'
+                    type='text'
+                  />
+                </div>
+                <label className='col-sm-2 control-label text-right'>地點</label>
+                <div className='col-4'>
+                  <input
+                    value={props.payload.location || ''}
+                    onChange={e => props.updatePayload({ location: e.target.value })}
+                    className='form-control'
+                    placeholder='必填'
+                    type='text'
+                  />
+                </div>
+              </div>
+            </div>
+            <div className='form-group'>
+              <div className='row'>
+                <label className='col-sm-2 control-label text-right'>開始時間</label>
+                <div className='col-4'>
+                  <input
+                    value={props.payload.begin_time || ''}
+                    onChange={e => props.updatePayload({begin_time: e.target.value})}
+                    className='form-control'
+                    placeholder='必填'
+                    type='date'
+                  />
+                </div>
+                <label className='col-sm-2 control-label text-right'>結束時間</label>
+                <div className='col-4'>
+                  <input
+                    value={props.payload.end_time || ''}
+                    onChange={e => props.updatePayload({end_time: e.target.value})}
+                    className='form-control'
+                    placeholder='必填'
+                    type='date'
+                  />
+                </div>
+              </div>
+            </div>
+            <LabeledInput label='內容'>
+              <CKEditor
+                activeClass='p10'
+                content={props.payload.content || ''}
+                events={{change: (e) => props.updatePayload({ content: e.editor.getData() })}}
+              />
+            </LabeledInput>
+            <div className='col-12 text-right'>
+              <button type='submit' className='btn btn-success btn-large' onClick={props.onSubmit}>送出</button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </PageWrapper>
+)
+
+export default enhance(New)
