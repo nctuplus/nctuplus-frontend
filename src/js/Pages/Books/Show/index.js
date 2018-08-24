2bc514ee1775b029f1b22131162d6fa22b0b42f3
Chih-Yu <lcd010308@gmail.com>
2018-08-24 17:03:10 +0800

    Change Show page to class and little fix

--- src/js/Pages/Books/Show/index.js
+++ src/js/Pages/Books/Show/index.js
@@ -4,65 +4,76 @@ import PageWrapper from '../../../Components/PageWrapper'
 import './style.scss'
 
 import { connect } from 'react-redux'
+import { withRouter } from 'react-router'
 import { getBook } from '../../../Redux/Actions/Books'
 
-const Show = ({ match, book, status, getBook, ...props }) => {
-  status || getBook(match.params.id)
-  return (
-    <PageWrapper>
-      <div className='container'>
-        <div className='row bg-white'>
-          <div className='col-12 col-md-11 offset-md-1'>
-            <div className='row'>
-              <div className='col-12 col-md-3 offset-md-1'>
-                <img className='img-preview' alt='尚無圖片!' src='https://plus.nctu.edu.tw/file_upload/book_covers/000/001/432/IMG_20170921_151626.jpg?1506278647' />
-              </div>
-              <div className='col-8'>
-                <table className='table'>
-                  <tbody>
-                    <tr>
-                      <td colSpan='2' className='col-md-12'>
-                        <h4><strong>{ book.name }</strong></h4>
-                        by { book.author }
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>價格</td>
-                      <td><span className='price'>{ book.price }</span></td>
-                    </tr>
-                    <tr>
-                      <td>賣家</td>
-                      <td>{ book.seller && book.seller.name }</td>
-                    </tr>
-                    <tr>
-                      <td>連絡方式</td>
-                      <td><a href='#'>Facebook</a></td>
-                    </tr>
-                    <tr>
-                      <td>書況</td>
-                      <td className='line-break'>{ book.description }</td>
-                    </tr>
-                    <tr>
-                      <td>觀看次數</td>
-                      <td>{ book.view_count }</td>
-                    </tr>
-                    <tr>
-                      <td>ISBN</td>
-                      <td>{ book.isbn }</td>
-                    </tr>
-                    <tr>
-                      <td>適用課程</td>
-                      <td />
-                    </tr>
-                  </tbody>
-                </table>
+class Show extends React.Component {
+  componentDidMount () {
+    this.props.getBook(this.props.match.params.id)
+  }
+
+  render () {
+    return (
+      <PageWrapper>
+        <div className='container'>
+          <div className='row bg-white'>
+            <div className='col-12 col-md-11 offset-md-1'>
+              <div className='row'>
+                <div className='col-12 col-md-3 offset-md-1'>
+                  <img
+                    className='img-preview'
+                    alt='尚無圖片!'
+                    width='100%'
+                    src={this.props.book.poster}
+                  />
+                </div>
+                <div className='col-8'>
+                  <table className='table'>
+                    <tbody>
+                      <tr>
+                        <td colSpan='2' className='col-md-12'>
+                          <h4><strong>{ this.props.book.name }</strong></h4>
+                          by { this.props.book.authors }
+                        </td>
+                      </tr>
+                      <tr>
+                        <td>價格</td>
+                        <td><span className='price'>{ this.props.book.price }</span></td>
+                      </tr>
+                      <tr>
+                        <td>賣家</td>
+                        <td>{ this.props.book.seller && this.props.book.seller.name }</td>
+                      </tr>
+                      <tr>
+                        <td>連絡方式</td>
+                        <td><a href='#'>Facebook</a></td>
+                      </tr>
+                      <tr>
+                        <td>書況</td>
+                        <td className='line-break'>{ this.props.book.description }</td>
+                      </tr>
+                      <tr>
+                        <td>觀看次數</td>
+                        <td>{ this.props.book.view_count }</td>
+                      </tr>
+                      <tr>
+                        <td>ISBN</td>
+                        <td>{ this.props.book.isbn }</td>
+                      </tr>
+                      <tr>
+                        <td>適用課程</td>
+                        <td />
+                      </tr>
+                    </tbody>
+                  </table>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </PageWrapper>
-  )
+      </PageWrapper>
+    )
+  }
 }
 
 const mapStateToProps = (state) => ({
@@ -74,4 +85,4 @@ const mapDispatchToProps = (dispatch) => ({
   getBook: (id) => dispatch(getBook(id))
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(Show)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Show))
