9003df1cf05fe3030bfc607707e38531da2230f9
Chih-Yu <lcd010308@gmail.com>
2018-09-03 23:41:03 +0800

    Change background Index to class

--- src/pages/Admin/Background/_Index/index.js
+++ src/pages/Admin/Background/_Index/index.js
@@ -1,10 +1,28 @@
 
 import React from 'react'
+import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import Bulletin from 'components/Admin/Bulletin'
+import { fetchBackgrounds } from 'redux/Actions/Backgrounds'
 
-const Index = props => (
-  <Bulletin type='background' url={props.match.url} />
-)
+const mapStateToProps = (state) => ({
+  backgrounds: state.backgrounds.all
+})
 
-export default withRouter(Index)
+const mapDispatchToProps = (dispatch) => ({
+  fetchData: () => dispatch(fetchBackgrounds())
+})
+
+class Index extends React.Component {
+  componentDidMount () {
+    this.props.fetchData()
+  }
+
+  render () {
+    return (
+      <Bulletin type='background' url={this.props.match.url} data={this.props.backgrounds.data} />
+    )
+  }
+}
+
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Index))
