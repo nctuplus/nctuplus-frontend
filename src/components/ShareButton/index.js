7500c0ae96277e398fd5494e6903b3adfd0e33b6
yinghua <shadow10230@gmail.com>
2018-09-13 12:51:00 +0800

    css-module(20/34)

--- src/components/ShareButton/index.js
+++ src/components/ShareButton/index.js
@@ -1,7 +1,7 @@
 
 import React from 'react'
 import Modal from 'components/Modal'
-import './style.scss'
+import styles from './style.scss'
 
 import { connect } from 'react-redux'
 
@@ -25,14 +25,14 @@ const ShareModal = connect(mapStateToProps)((props) => {
       <div className='modal-header'>
         <h5 className='modal-title'>分享</h5>
       </div>
-      <div className='modal-body'>
-        <a className='btn' href={`https://www.facebook.com/sharer/sharer.php?u=${shareURL}`} target='_blank'><i className='fa fa-facebook-square' /></a>
-        <a className='btn' href={`https://plus.google.com/share?url=${shareURL}`} target='_blank'><i className='fa fa-google-plus-square' /></a>
-        <a className='btn' href={`https://twitter.com/home?status=${shareURL}`} target='_blank'><i className='fa fa-twitter-square' /></a>
+      <div className={`modal-body ${styles.modalBody}`}>
+        <a className='btn' href={`https://www.facebook.com/sharer/sharer.php?u=${shareURL}`} target='_blank'><i className={`fa fa-facebook-square ${styles.facebook}`} /></a>
+        <a className='btn' href={`https://plus.google.com/share?url=${shareURL}`} target='_blank'><i className={`fa fa-google-plus-square ${styles.google}`} /></a>
+        <a className='btn' href={`https://twitter.com/home?status=${shareURL}`} target='_blank'><i className={`fa fa-twitter-square ${styles.twitter}`} /></a>
         <hr />
         <p className='input-group'>
           <span id='shareURL' className='input-group-prepend input-group-text mr-3'>{`https://plus.nctu.edu.tw/shares/${hash}`}</span>
-          <span className='btn'><i className='fa fa-copy' /></span>
+          <span className='btn'><i className={`fa fa-copy ${styles.copy}`} /></span>
         </p>
       </div>
     </Modal>
