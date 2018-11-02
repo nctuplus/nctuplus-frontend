6ded84326ac8d0da89a13a60c8718ebe06b27065
Chih-Yu <lcd010308@gmail.com>
2018-11-02 22:49:07 +0800

    Add missing image and type in Events#Show

--- src/pages/Events/Show/index.js
+++ src/pages/Events/Show/index.js
@@ -23,19 +23,43 @@ class Show extends React.Component {
 
   render () {
     const { event, followEvents } = this.props
-    const index = followEvents.findIndex(e => e.id === event.id)
+    const followIndex = followEvents.findIndex(e => e.id === event.id)
+    let type
+    switch (event.event_type) {
+      case 1:
+        type = '校友週'
+        break
+      case 2:
+        type = '表演'
+        break
+      case 3:
+        type = '講座'
+        break
+      case 4:
+        type = '擺攤'
+        break
+      case 5:
+        type = '比賽'
+        break
+      case 6:
+        type = '其他'
+        break
+      default:
+        type = ''
+        break
+    }
 
     return (
       <Layout>
         <div className='container'>
           <div className={styles.bannerWrapper}>
             <img
-              alt='Banner with text'
+              alt='No image'
               width='100%'
-              src={event.poster}
+              src={event.cover_image && `${SERVER_URL}${event.cover_image.url}`}
             />
           </div>
-          <span className={styles.eventTitle}>{event.title}</span>
+          <span className={styles.eventTitle}>[{type}] {event.title}</span>
           <div className={`${styles.eventInfoWrapper} bg-white`}>
             <div className='row'>
               <div className='col-7'>
@@ -56,31 +80,31 @@ class Show extends React.Component {
             </div>
             <section dangerouslySetInnerHTML={{ __html: event.content }} />
           </div>
-        </div>
-        <div className={`${styles.fixedMenu} fixed`}>
-          <div className='container'>
-            { // 這裡先直接顯示 之後要改成判斷是否為自己的活動
-              <div className='pull-left'>
-                <Link to={`/events/${this.props.match.params.id}/edit`} className='flat-link'>
-                  <button className='btn btn-primary nav-button' >
-                    編輯
+          <div className='row mx-0 py-3'>
+            <div className={`${styles.fixedMenu}`}>
+              { // 這裡先直接顯示 之後要改成判斷是否為自己的活動
+                <div className='pull-left'>
+                  <Link to={`/events/${this.props.match.params.id}/edit`} className='flat-link'>
+                    <button className='btn btn-primary nav-button' >
+                      編輯
+                    </button>
+                  </Link>
+                  <button className='btn btn-danger nav-button' onClick={() => this.props.deleteEvent(this.props.match.params.id)}>
+                    刪除
                   </button>
-                </Link>
-                <button className='btn btn-danger nav-button' onClick={() => this.props.deleteEvent(this.props.match.params.id)}>
-                  刪除
-                </button>
-              </div>
-            }
-            <div className='pull-right'>
-              {
-                index >= 0
-                  ? <button className='btn btn-success nav-button' onClick={() => this.props.cancelFollow(event.id, index, followEvents)}>
-                  取消關注
-                  </button>
-                  : <button className='btn btn-success nav-button' onClick={() => this.props.follow(event.id, followEvents)}>
-                  關注
-                  </button>
+                </div>
               }
+              <div className='pull-right'>
+                {
+                  followIndex >= 0
+                    ? <button className='btn btn-success nav-button' onClick={() => this.props.cancelFollow(event.id, followIndex, followEvents)}>
+                    取消關注
+                    </button>
+                    : <button className='btn btn-success nav-button' onClick={() => this.props.follow(event.id, followEvents)}>
+                    關注
+                    </button>
+                }
+              </div>
             </div>
           </div>
         </div>
