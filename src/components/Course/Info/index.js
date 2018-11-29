1236cd50c933f6c4c3790fe081dc22b1a425ee6f
yinghua <shadow10230@gmail.com>
2018-11-29 10:24:58 +0800

    waiting for backend

--- src/components/Course/Info/index.js
+++ src/components/Course/Info/index.js
@@ -1,56 +1,107 @@
 
 import React from 'react'
 import { convertSemesterToString, convertTimeSlotsToString } from 'utilities'
+import styles from './style.scss';
+import classNames from 'classnames';
 
-const Info = (props) => (
-  <div className='row p-4'>
-    <table className='table-invisible'>
-      <tbody>
-        <tr>
-          <td>永久課號<strong>{props.permanent_course.code}</strong></td>
-          <td>學分<strong>{props.credit}</strong></td>
-        </tr>
-        <tr>
-          <td>
-            <a href={props.href} target='_blank'>課程綱要</a>
-          </td>
-          <td>
-            查看更多<i className='fa fa-search-plus' />
-          </td>
-        </tr>
-      </tbody>
-    </table>
-    <table className='table'>
-      <thead>
-        <tr>
-          <th>學期</th>
-          <th>單位</th>
-          <th>課號</th>
-          <th>選別</th>
-          <th>修課人數/上限</th>
-          <th>時間</th>
-          <th>教室</th>
-          <th>年級</th>
-          <th>備註</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{convertSemesterToString(props.semester)}</td>
-          <td>{props.department}</td>
-          <td>
-            <a href={props.href} target='_blank'>{props.code}</a>
-          </td>
-          <td>{props.requirement_type}</td>
-          <td className='text-center'>{props.registration_count}/{props.registration_limit}</td>
-          <td>{convertTimeSlotsToString(props.time_slots)}</td>
-          <td>{props.classroom}</td>
-          <td>{props.grade}</td>
-          <td>{props.remark}</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-)
+class Info extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      tableFolded:true
+    }
+  }
+  render(){
+    const course_infos = this.props.course_infos || [];
+    const 當期課號 = course_infos[0]?course_infos[0].code:"";
+    return(
+      <div className="container">
+        <div className="row">
+          <table className={classNames('table', styles.tableBorderless, "col-md-6")}>
+            <tbody>
+              <tr>
+                <td>當期課號：<strong>{當期課號}</strong></td>
+                <td>學分：<strong>{this.props.permanent_course.credit}</strong></td>
+              </tr>
+              <tr>
+                <td>
+                  永久課號：<strong>{this.props.permanent_course.code}</strong>
+                </td>
+                <td>
+                  <a href={this.props.href} target='_blank'>課程綱要 <i className="fas fa-external-link-alt"></i></a>
+                </td>
+
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className="table-responsive">
+          <table className='table table-striped table-hover'>
+            <thead>
+              <tr>
+                <th>學期</th>
+                <th>單位</th>
+                <th>當期課號</th>
+                <th>選別</th>
+                <th>修課人數/上限</th>
+                <th>時間</th>
+                <th>教室</th>
+                <th>學分</th>
+                <th>年級</th>
+                <th>備註</th>
+              </tr>
+            </thead>
+            <tbody>
+              {course_infos.map((item, index)=>{
+                let trStyle = "";
+
+                const semester = course_infos[0]?course_infos[0].semester : {}
+                if((JSON.stringify(item.semester) !== JSON.stringify(semester)) && this.state.tableFolded){
+                  trStyle = styles.fold;
+                }
+                return (
+                  <tr key={index} className = {trStyle}>
+                    <td>{convertSemesterToString(item.semester)}</td>
+                    <td>{item.department}</td>
+                    <td>{item.code}</td>
+                    <td>{item.requirement_type}</td>
+                    <td className='text-center'>{item.registration_count}/{item.registration_limit}</td>
+                    <td>{convertTimeSlotsToString(item.time_slots)}</td>
+                    <td>{item.classroom}</td>
+                    <td>{item.grade}</td>
+                    <td>{item.remark}</td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <td>{convertSemesterToString(this.props.semester)}</td>
+                <td>{this.props.department}</td>
+                <td>
+                  <a href={this.props.href} target='_blank'>{this.props.code}</a>
+                </td>
+                <td>{this.props.requirement_type}</td>
+                <td className='text-center'>{this.props.registration_count}/{this.props.registration_limit}</td>
+                <td>{convertTimeSlotsToString(this.props.time_slots)}</td>
+                <td>{this.props.classroom}</td>
+                <td>{this.props.credit}</td>
+                <td>{this.props.grade}</td>
+                <td>{this.props.remark}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div className={styles.moreIcon}>
+          <div>
+            <span>查看往年資訊</span>
+            <div className={styles.angleContainer}>
+              <span>&#65088;</span>
+              <span className={styles.downAngle}>&#65088;</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Info
--- src/components/Course/Tips/index.js
+++ src/components/Course/Tips/index.js
@@ -4,9 +4,9 @@ import React from 'react'
 // @todo: connect API
 const Tips = () => (
   <div className='panel-body'>
-    <i className='fa fa-cube' /> 作業/考試：<a>編輯</a>
+    {/* <i className='fa fa-cube' /> 作業/考試：<a>編輯</a>
     <div>
-      考試週：（該週可能有考試）
+      {/* 考試週：（該週可能有考試）
       <div className='btn-group'>
         {
           new Array(18).fill(0).map((value, index) =>
@@ -28,31 +28,38 @@ const Tips = () => (
         }
       </div>
       <br />
-      <i className='fa fa-eye' />點名頻率：
+
+    </div> */}
+    <div className='my-2'>
+      <i className='fa fa-eye mx-2' />點名頻率：
       <span className='label label-success rollcall'>不點名</span>
     </div>
-    <hr />
-    <i className='fa fa-cube' />其他內容：
-    <a className='edit-lists'>編輯</a>
-    <div className='row d-none'>
-      <div className='col-4'>
-        <select className='form-control'>
-          <option value='1'>[考試]</option>
-          <option value='2'>[作業]</option>
-          <option value='3'>[上課]</option>
-          <option value='4'>[其他]</option>
-        </select>
-      </div>
-      <div className='col-7'>
-        <input className='form-control' maxLength='32' type='text' />
-      </div>
-      <div className='col-1'>
-        <button className='btn btn-success btn-circle'>
-          <i className='fa fa-check' />
-        </button>
+    <div className='my-2'>
+      <i className='fa fa-cube mx-2' />其他內容：
+      <a className='edit-lists'>編輯</a>
+      <div className='row d-none'>
+        <div className='col-4'>
+          <select className='form-control'>
+            <option value='1'>[考試]</option>
+            <option value='2'>[作業]</option>
+            <option value='3'>[上課]</option>
+            <option value='4'>[其他]</option>
+          </select>
+        </div>
+        <div className='col-7'>
+          <input className='form-control' maxLength='32' type='text' />
+        </div>
+        <div className='col-1'>
+          <button className='btn btn-success btn-circle'>
+            <i className='fa fa-check' />
+          </button>
+        </div>
       </div>
     </div>
-    <p className='text-center no-data'><strong>尚無資料</strong></p>
+    <div className='d-flex flex-column py-3 align-items-stretch'>
+      {/*** 這邊之後放資料 ***/}
+      <p className='text-center no-data my-0'><strong>尚無資料</strong></p>
+    </div>
   </div>
 )
 
