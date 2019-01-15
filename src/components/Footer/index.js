9b45fbf6c0aa452dd88136fe3b9c10ca29f99e48
yinghua <shadow10230@gmail.com>
2019-01-15 15:17:40 +0800

    beautified footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,16 +4,16 @@ import './style.scss'
 
 const Footer = (props) => (
   /* if current page is login page, the footer should be bottom-fixed. */
-  <footer>
-    <div className='text-center'>
+  <footer className='d-flex justify-content-center align-items-center'>
+    <div className='d-flex justify-content-center align-items-center'>
       <span className='mx-2'>© Copyright NCTU+ 2016</span>
-      <a href='https://www.facebook.com/nctuplus' target='_blank'>
+      <a className='d-inline-flex' href='https://www.facebook.com/nctuplus' target='_blank'>
         <i className='fa fa-2x fa-facebook-square mx-1' />
       </a>
-      <a href='https://github.com/nctuplus/nctuplus/' target='_blank'>
+      <a className='d-inline-flex' href='https://github.com/nctuplus/nctuplus/' target='_blank'>
         <i className='fa fa-2x fa-github-square mx-1' />
       </a>
-      <a href='mailto:nctuplus@gmail.com' target='_blank'>
+      <a className='d-inline-flex' href='mailto:nctuplus@gmail.com' target='_blank'>
         <i className='fa fa-envelope fa-2x mx-1' />
       </a>
     </div>
