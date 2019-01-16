606d6cab716d0c86d957e8ee3597e3023b294f5c
yinghua <shadow10230@gmail.com>
2019-01-16 23:40:12 +0800

    beautified footer (#40)
    * beautified footer
    
    * js dynamic edit copyright

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,22 +2,23 @@
 import React from 'react'
 import './style.scss'
 
-const Footer = (props) => (
+const Footer = (props) => {
   /* if current page is login page, the footer should be bottom-fixed. */
-  <footer>
-    <div className='text-center'>
-      <span className='mx-2'>© Copyright NCTU+ 2016</span>
-      <a href='https://www.facebook.com/nctuplus' target='_blank'>
-        <i className='fa fa-2x fa-facebook-square mx-1' />
-      </a>
-      <a href='https://github.com/nctuplus/nctuplus/' target='_blank'>
-        <i className='fa fa-2x fa-github-square mx-1' />
-      </a>
-      <a href='mailto:nctuplus@gmail.com' target='_blank'>
-        <i className='fa fa-envelope fa-2x mx-1' />
-      </a>
-    </div>
-  </footer>
-)
+  const dt = new Date()
+  const year = dt.getFullYear()
+  return (
+    <footer className='d-flex justify-content-center align-items-center'>
+      <div className='d-flex justify-content-center align-items-center'>
+        <span className='mx-2'>© Copyright NCTU+ {year}</span>
+        <a className='d-inline-flex' href='https://www.facebook.com/nctuplus' target='_blank'>
+          <i className='fa fa-2x fa-facebook-square mx-1' />
+        </a>
+        <a className='d-inline-flex' href='https://github.com/nctuplus/nctuplus/' target='_blank'>
+          <i className='fa fa-2x fa-github-square mx-1' />
+        </a>
+      </div>
+    </footer>
+  )
+}
 
 export default Footer
