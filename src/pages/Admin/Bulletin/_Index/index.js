46d86ce186373f3acb5aafbe62b9d3cf6614012a
Chih-Yu <lcd010308@gmail.com>
2019-01-18 02:20:09 +0800

    Add Admin Bulletin delete function

--- src/pages/Admin/Bulletin/_Index/index.js
+++ src/pages/Admin/Bulletin/_Index/index.js
@@ -3,24 +3,45 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import Bulletin from 'components/Admin/Bulletin'
-import { getBulletins } from 'api/Controllers/bulletins'
+import { getBulletins, deleteBulletin } from 'api/Controllers/bulletins'
+import actions from 'api/Actions/Bulletins'
+import { FETCHING_STATUS } from 'utilities/constants'
 
 const mapStateToProps = (state) => ({
-  bulletins: state.bulletins.index
+  bulletins: state.bulletins.index,
+  bulletinDeleteStatus: state.bulletins.delete.status
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchData: (payload) => dispatch(getBulletins(payload))
+  fetchBulletins: (payload) => dispatch(getBulletins(payload)),
+  deleteBulletin: (id) => {
+    if (window.confirm('確定刪除此活動嗎?')) {
+      dispatch(deleteBulletin(id))
+    }
+  },
+  deleteBulletinReset: () => dispatch(actions.bulletins.delete.setStatus(FETCHING_STATUS.IDLE))
 })
 
 class Index extends React.Component {
   componentDidMount () {
-    this.props.fetchData()
+    this.props.fetchBulletins()
+  }
+
+  componentDidUpdate () {
+    if (this.props.bulletinDeleteStatus === FETCHING_STATUS.DONE) {
+      this.props.deleteBulletinReset()
+      this.props.fetchBulletins()
+    }
   }
 
   render () {
     return (
-      <Bulletin type='bulletin' url={this.props.match.url} data={this.props.bulletins.data} />
+      <Bulletin
+        type='bulletin'
+        url={this.props.match.url}
+        data={this.props.bulletins.data}
+        onDelete={(id) => this.props.deleteBulletin(id)}
+      />
     )
   }
 }
