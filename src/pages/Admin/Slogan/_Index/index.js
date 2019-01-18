6b08f5882d96a0da942eb1d0a1527f95365f1697
Chih-Yu <lcd010308@gmail.com>
2019-01-18 14:11:24 +0800

    Add Admin Slogan delete function

--- src/pages/Admin/Slogan/_Index/index.js
+++ src/pages/Admin/Slogan/_Index/index.js
@@ -3,14 +3,23 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import Bulletin from 'components/Admin/Bulletin'
-import { getSlogans } from 'api/Controllers/slogans'
+import { getSlogans, deleteSlogan } from 'api/Controllers/slogans'
+import actions from 'api/Actions/Slogans'
+import { FETCHING_STATUS } from 'utilities/constants'
 
 const mapStateToProps = (state) => ({
-  slogans: state.slogans.index
+  slogans: state.slogans.index,
+  sloganDeleteStatus: state.slogans.delete.status
 })
 
 const mapDispatchToProps = (dispatch) => ({
-  fetchData: () => dispatch(getSlogans())
+  fetchData: () => dispatch(getSlogans()),
+  deleteSlogan: (id) => {
+    if (window.confirm('確定刪除此活動嗎?')) {
+      dispatch(deleteSlogan(id))
+    }
+  },
+  deleteSloganReset: () => dispatch(actions.slogans.delete.setStatus(FETCHING_STATUS.IDLE))
 })
 
 class Index extends React.Component {
@@ -18,9 +27,21 @@ class Index extends React.Component {
     this.props.fetchData()
   }
 
+  componentDidUpdate () {
+    if (this.props.sloganDeleteStatus === FETCHING_STATUS.DONE) {
+      this.props.deleteSloganReset()
+      this.props.fetchData()
+    }
+  }
+
   render () {
     return (
-      <Bulletin type='slogan' url={this.props.match.url} data={this.props.slogans.data} />
+      <Bulletin
+        type='slogan'
+        url={this.props.match.url}
+        data={this.props.slogans.data}
+        onDelete={(id) => this.props.deleteSlogan(id)}
+      />
     )
   }
 }
