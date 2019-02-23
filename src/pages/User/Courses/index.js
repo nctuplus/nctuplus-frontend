889953c77cca4095315d05bdeec0d3608264459d
ourobin2 <ozxing99@gmail.com>
2019-02-23 14:34:26 +0800

    add history course page

--- src/pages/User/Courses/index.js
+++ src/pages/User/Courses/index.js
@@ -1,13 +1,15 @@
 
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import classNames from 'classnames'
+import styles from './index.scss'
 const CoursesYearlyStatistic = (props) => (
-  <div className='col-12 col-md-6 col-lg-4'>
-    <h4 className='text-center'>{ props.year }</h4>
-    <table className='table table-bordered border-thick-gray'>
+  <div className={classNames('mt-5 ml-4', styles.table)}>
+    <table className={classNames('table table-bordered border-thick-gray', styles.table)}>
       <thead>
         <tr>
-          <td className='text-center'>{ props.semaster }</td>
+          <td className='text-center'>{ props.semester }</td>
+          <td className='w-12' />
           <td className='text-center'><i className='fa fa-graduation-cap' /></td>
           <td className='text-center'><i className='fa fa-flag-checkered' /></td>
         </tr>
@@ -16,25 +18,38 @@ const CoursesYearlyStatistic = (props) => (
         {
           props.courses.map((course, index) => (
             <tr>
-              <td>{ course.name } | { course.property }</td>
-              <td className='text-center'>{ course.credit }</td>
-              <td className='text-center bold'>{ course.score }</td>
+              <td >{course.name} | {course.type}</td>
+              <td >
+                <button className='btn-warning mr-1 ' type='button'>
+                  <Link className='flat-link text-white' to='/user/edit'>
+                    評論
+                  </Link>
+                </button>
+                <button className='btn-primary' type='button'>
+                  <Link className='flat-link text-white' to='/user/edit'>
+                    上傳考古
+                  </Link>
+                </button>
+              </td>
+              <td className='text-center'>{ course.credit}</td>
+              <td className='text-center bold'>{course.score === '未通過' ? <span style={{ color: '#FF0000' }}>未通過</span> : course.score === 'W' ? <span style={{ color: '#FF0000' }}>W</span> : course.score}</td>
             </tr>
           ))
         }
-
-        <tr className='info'>
-          <td className='text-right'>共</td>
+        <tr className='info' style={{ backgroundColor: '#99BBFF' }}>
+          <td colSpan='2' className='text-right text-black'>總計</td>
           <td className='text-center'>
             {
               /* all credit */
-              props.courses.reduce((accum, course) => accum + course.credit)
+              props.total_credit
+              /* props.courses.reduce((accum, course) => accum + course.credit) */
             }
           </td>
           <td className='text-center bold'>
             {
               /* average score */
-              props.courses.reduce((accum, course) => accum + course.score) / props.course.length
+              props.average
+              /* props.courses.reduce((accum, course) => accum + course.score) / props.course.length */
             }
           </td>
         </tr>
@@ -43,14 +58,150 @@ const CoursesYearlyStatistic = (props) => (
   </div>
 )
 
+const tempCourses =
+  [
+    {
+      'semester': '106下',
+      'total_credit': 21,
+      'average': 90,
+      'courses': [
+        {
+          'name': '服務學習(二)',
+          'code': 'DCP3362',
+          'type': '基本必修',
+          'score': '未通過',
+          'credit': 0
+        },
+        {
+          'name': '經濟社會學',
+          'code': 'GEC8174',
+          'type': '群己/進階(96 )',
+          'score': 90,
+          'credit': 2
+        },
+        {
+          'name': '計算機系統',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        },
+        {
+          'name': '計算機系統',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        },
+        {
+          'name': '計算機系統',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        },
+        {
+          'name': '計算機系統',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        },
+        {
+          'name': '資料結構與',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        },
+        {
+          'name': '計算機系統',
+          'code': 'GEC8174',
+          'type': '基本必修',
+          'score': 90,
+          'credit': 3
+        }
+      ]
+    },
+    {
+      'semester': '106上',
+      'total_credit': 20,
+      'average': 87.5,
+      'courses': [
+        {
+          'name': '計算機組織',
+          'code': 'DCP3362',
+          'type': '基本必修',
+          'score': 95,
+          'credit': 3
+        },
+        {
+          'name': '經濟社會學',
+          'code': 'GEC8174',
+          'type': '群己/進階(96 )',
+          'score': 90,
+          'credit': 2
+        }
+      ]
+    },
+    {
+      'semester': '106上',
+      'total_credit': 20,
+      'average': 87.5,
+      'courses': [
+        {
+          'name': '計算機組織',
+          'code': 'DCP3362',
+          'type': '基本必修',
+          'score': 95,
+          'credit': 3
+        },
+        {
+          'name': '經濟社會學',
+          'code': 'GEC8174',
+          'type': '群己/進階(96 )',
+          'score': 90,
+          'credit': 2
+        }
+      ]
+    },
+    {
+      'semester': '106上',
+      'total_credit': 20,
+      'average': 87.5,
+      'courses': [
+        {
+          'name': '計算機組織',
+          'code': 'DCP3362',
+          'type': '基本必修',
+          'score': 95,
+          'credit': 3
+        },
+        {
+          'name': '經濟社會學',
+          'code': 'GEC8174',
+          'type': '群己/進階(96 )',
+          'score': 90,
+          'credit': 2
+        }
+      ]
+    }
+  ]
+
 const Courses = (props) => (
-  <div className='bg-white'>
+  <div className={classNames('bg-white', styles.index)}>
     <div className='row no-margin' >
       {
-        props.yearlyStatistics &&
-        props.yearlyStatistics.map((data, index) => (
-          <CoursesYearlyStatistic {...data} key={index} />
-        ))
+        // props.yearlyStatistics &&
+        // props.yearlyStatistics.map((data, index) => (
+        //   <CoursesYearlyStatistic {...data} key={index} />
+        // ))
+      }
+      {
+        tempCourses &&
+         tempCourses.map((data, index) => (
+           <CoursesYearlyStatistic {...data} key={index} />
+         ))
       }
     </div>
   </div>
