b2f2435e3696b96828a914d593a5889cecc3fe21
yinghua <shadow10230@gmail.com>
2019-03-08 16:08:10 +0800

    remove font-size from root, and new design for comments

--- src/components/Comment/Table/index.js
+++ src/components/Comment/Table/index.js
@@ -3,52 +3,47 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import Pagination from 'components/Pagination'
 import Spinner from 'components/Spinner'
+import styles from './style.scss'
 
-const CommentsTableRow = withRouter((props) => (
-  <tr className='clickable' onClick={() => props.history.push(`/comments/${props.id}`)}>
-    <td>
-      { props.course && props.course.name }
-      /
-      { props.course &&
-        props.course.teachers.length
-        ? <React.Fragment>
-          { props.course.teachers[0] }
-          { props.course.teachers.slice(1).map((name) => `,${name}`) }
-        </React.Fragment>
-        : 'N/A'
-      }
-    </td>
-    <td>{props.title}</td>
-    <td className='d-none d-table-cell'>{ props.anonymity ? '匿名' : props.user.name }</td>
-    <td className='d-none d-table-cell'>{ props.created_at && props.created_at.substr(0, 10) }</td>
-  </tr>
+const CommentsCard = withRouter((props) => (
+  <div className={`clickable card ${styles.card}`}>
+    <div className='card-body'>
+      <div className={styles.header}>
+        <div className={`${styles.user}`}><i className='fas fa-user-circle mr-2' />{ props.anonymity ? '匿名' : props.user.name }</div>
+        <div className={` text-muted ${styles.time}`}>{ props.created_at && props.created_at.substr(0, 10) }</div>
+        <div className={`text-secondary ${styles.cardSubtitle}`}>
+          { props.course && props.course.name }
+            /
+          { props.course &&
+              props.course.teachers.length
+            ? <React.Fragment>
+              { props.course.teachers[0] }
+              { props.course.teachers.slice(1).map((name) => `,${name}`) }
+            </React.Fragment>
+            : 'N/A'
+          }
+        </div>
+      </div>
+      <h5 className={styles.cardTitle}>{props.title}</h5>
+      <div className={`text-muted ${styles.cardText}`}>{props.content}</div>
+
+    </div>
+  </div>
 ))
 
 const CommentsTable = (props) => (
   <div>
-    <table className='table table-hover bg-white'>
-      <thead>
-        <tr className='clickable'>
-          <th>課程/教授</th>
-          <th>標題</th>
-          <th className='d-none d-table-cell'>作者</th>
-          <th className='d-none d-table-cell'>時間</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          props.data.length
-            ? props.data.map((comment) => (
-              <CommentsTableRow key={comment.id} {...comment} />
-            ))
-            : <tr className='text-center'>
-              <td colSpan='4'>
-                <Spinner size={48} color='grey' />
-              </td>
-            </tr>
-        }
-      </tbody>
-    </table>
+    <div className='container mb-3'>
+      {
+        props.data.length
+          ? props.data.map((comment) => (
+            <CommentsCard key={comment.id} {...comment} />
+          ))
+          : <div>
+            <Spinner size={48} color='grey' />
+          </div>
+      }
+    </div>
     <div className='text-center'>
       <Pagination page={props.page} maxPage={props.maxPage} to={props.updatePage} />
     </div>
